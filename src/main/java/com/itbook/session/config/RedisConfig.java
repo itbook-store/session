accb7e059526a9fe2c7ec20fd77849fea6ed058b
yihoney <109622@naver.com>
2023-01-17 00:44:04 +0900

    [feat] : 인터셉터 추가

--- src/main/java/com/itbook/session/config/RedisConfig.java
+++ src/main/java/com/itbook/session/config/RedisConfig.java
@@ -1,39 +1,44 @@
 package com.itbook.session.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.BeanClassLoaderAware;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * redis 에대한 설정 파일입니다.
+ *
+ * @author 이하늬
+ * @since 1.0
+ */
 @Configuration
-@PropertySource("classpath:redis.properties")
-@EnableRedisRepositories
-public class RedisConfig {
-    @Value("${spring.redis.host}")
+@ConfigurationProperties(prefix = "redis")
+public class RedisConfig implements BeanClassLoaderAware {
     private String host;
-
-    @Value("${spring.redis.port}")
     private int port;
-
+    private String password;
+    private int database;
+    private ClassLoader classLoader;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPort(port);
+        configuration.setHostName(this.host);
+        configuration.setPort(this.port);
+        configuration.setPassword(this.password);
+        configuration.setDatabase(this.database);
+
         return new LettuceConnectionFactory(configuration);
     }
 
+    @SuppressWarnings("java:S1452") // 레디스의 key value의 타입을 자유롭게 지정하기 위함.
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
@@ -46,4 +51,44 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(int database) {
+        this.database = database;
+    }
+
+    public ClassLoader getClassLoader() {
+        return classLoader;
+    }
 }
--- src/main/java/com/itbook/session/controller/LoginController.java
+++ src/main/java/com/itbook/session/controller/LoginController.java
@@ -1,10 +1,14 @@
 package com.itbook.session.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,25 +19,40 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 1.0
  */
 @Controller
+@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/login")
+@RequestMapping("/")
 public class LoginController {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final String username_itbook = "itbook";
+    private final String pwd_itbook = "1234";
 
-    @GetMapping
+
+    @GetMapping("/login")
     public String getLoginForm() {
         return "login";
     }
 
-    @PostMapping
+    @GetMapping
+    public String getIndex(@CookieValue(value = "username", required = false) String username) {
+        log.info("{}", username);
+        return "index";
+    }
+
+    @PostMapping("/login")
     public String doLogin(HttpServletRequest request,
-                          @RequestParam String username, @RequestParam String pwd) {
+                          @RequestParam String username, @RequestParam String pwd,
+                          HttpServletResponse response) {
 
-        HttpSession session = request.getSession(false);
-        redisTemplate.opsForHash().put(session.getId(), "username", username);
-        session.setAttribute("username", username);
-
+        if (username.equals(username_itbook) && pwd.equals(pwd_itbook)) {
+            HttpSession session = request.getSession(false);
+            Cookie cookie = new Cookie("itbook-cookie", session.getId());
+            response.addCookie(cookie);
+            redisTemplate.opsForHash().put(session.getId(), "username", username);
+            session.setAttribute(session.getId(), username);
+        }
         return null;
     }
+
 }
